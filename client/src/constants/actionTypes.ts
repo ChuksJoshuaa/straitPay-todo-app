e650cfad42328a32ea5f88bf2b5844887b8685c5
Chuck Joshua Mbanaso <chuksjoshuaa@gmail.com>
2023-08-26 10:37:28 +0100

    Route url implemented

--- client/src/constants/actionTypes.ts
+++ client/src/constants/actionTypes.ts
@@ -2,5 +2,5 @@ export const LOADING = "LOADING";
 export const SEARCHDATA = "SEARCHDATA";
 export const OPENSIDEBAR = "OPENSIDEBAR";
 export const SETSCREEN = "SETSCREEN";
-export const LIVE_BASE_URL = "";
+export const LIVE_BASE_URL = "https://todo-api-server.vercel.app";
 export const DEV_BASE_URL = "http://localhost:5000";
--- client/src/routes/index.ts
+++ client/src/routes/index.ts
@@ -1,6 +1,8 @@
 import { DEV_BASE_URL, LIVE_BASE_URL } from "../constants/actionTypes";
 
-export const serverUrl = (env: string) => {
+const Url = (env: string) => {
   if (env === "development") return DEV_BASE_URL;
   return LIVE_BASE_URL;
 };
+
+export const serverUrl = Url(import.meta.env.VITE_NODE_ENV);
