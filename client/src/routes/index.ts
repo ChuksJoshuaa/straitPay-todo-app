389434d928f21dbda36f2fc55ff0ce1417c6bf01
Chuck Joshua Mbanaso <chuksjoshuaa@gmail.com>
2023-08-27 03:00:29 +0100

    Fix route issues

--- client/src/routes/index.ts
+++ client/src/routes/index.ts
@@ -1,8 +1,8 @@
 import { DEV_BASE_URL, LIVE_BASE_URL } from "../constants/actionTypes";
 
 const Url = (env: string) => {
-  if (env === "development") return DEV_BASE_URL;
-  return LIVE_BASE_URL;
+  if (env !== "development") return LIVE_BASE_URL;
+  else return DEV_BASE_URL;
 };
 
 export const serverUrl = Url(import.meta.env.VITE_NODE_ENV);
